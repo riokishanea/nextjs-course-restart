d054d0a37ac99e58f7c009ff8582c16eba00e3b9
riokishanea <workflow.riokishan@gmail.com>
2026-06-25 14:13:28 +0530

    Implement Answer Model : Initial

---
+++ database/answer.models.ts
@@ -0,0 +1,23 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IAnswer {
+  author: Types.ObjectId;
+  question: Types.ObjectId;
+  content: string;
+  upvote: number;
+  downvote: number;
+}
+
+const AnswerScheme = new Schema<IAnswer>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+    content: { type: String, required: true },
+    upvote: { type: Number,default:0, required: true },
+    downvote: { type: Number, default: 0, required: true },
+  },
+  { timestamps: true }
+);
+const Answer = models?.Answer || model<IAnswer>("Answer", AnswerScheme);
+
+export default Answer;
