c5b4870c396bfdd8d87ba3d87ea9883c53556619
riokishanea <workflow.riokishan@gmail.com>
2026-06-25 14:47:06 +0530

    typo fix: user.models.ts (Not sure)

--- database/user.model.ts
+++ database/user.model.ts
@@ -26,5 +26,5 @@ const UserSchema = new Schema<IUser>(
   { timestamps: true }
 );
 
-const User = models?.user || model<IUser>("User", UserSchema);
+const User = models?.User || model<IUser>("User", UserSchema);
 export default User;
