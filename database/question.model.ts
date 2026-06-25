43bde46e74f3d4f5abe00fb194feb479f24c9cca
riokishanea <workflow.riokishan@gmail.com>
2026-06-25 11:49:33 +0530

    Implement Question Model : Initial

---
+++ database/question.model.ts
@@ -0,0 +1,26 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IQuestion {
+    title: string;
+    content: string;
+    tags: Types.ObjectId[];
+    author: Types.ObjectId;
+    views: number;
+    upvotes: number;
+    downvotes: number;
+    answers: number;
+}
+
+const QuestionScheme = new Schema<IQuestion>({
+    title: {type: String, required: true},
+    content: {type: String, required: true}, 
+    tags: [{type: Schema.Types.ObjectId, ref: "Tags"}],
+    author: {type: Schema.Types.ObjectId, ref: "User"},
+    views: {type: Number, default: 0},
+    upvotes: {type: Number, default: 0}, 
+    downvotes: {type: Number, default: 0},
+    answers: {type: Number, default: 0},
+},{timestamps:true});
+
+const Question = models?.Question || model<IQuestion>("Question", QuestionScheme);
+export default Question;
